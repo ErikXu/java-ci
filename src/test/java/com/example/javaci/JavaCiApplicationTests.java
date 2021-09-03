93ac9de3b89fadc0289695fedb195e73012e38a4
ErikXu <81636759@qq.com>
2021-09-03 05:50:22 +0000

    Init.

---
+++ src/test/java/com/example/javaci/JavaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
