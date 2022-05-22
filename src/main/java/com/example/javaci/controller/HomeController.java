fb91f0dc6016ad77c7f54659f9370868dfede892
Erik Xu <81636759@qq.com>
2022-05-22 15:44:25 +0800

    Update HomeController.java

--- src/main/java/com/example/javaci/controller/HomeController.java
+++ src/main/java/com/example/javaci/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello ErikXu!";
     }
 }
